4890b3e7ca1eade619e053089f07e3d1b13eb9ed
logitha <loga848.456@gmail.com>
2016-07-09 09:55:15 +0530

    Create awa.c

---
+++ awa.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() 
+{
+	
+int a1[20],a2[20];
+	int i,j,n,k,c=0;
+	scanf("%d",&k);
+	scanf("%d",&n);
+	
+for(i=0;i<k;i++)
+	
+	scanf("%d",&a1[i]);
+	
+for(i=0;i<n;i++)
+	
+		scanf("%d",&a2[i]);
+	
+for(i=0;i<k;i++)
+
+ for(j=0;j<n;j++)
+ 
+  if(a1[i]==a2[j])
+ 
+      c++;
+ 
+             if(c==k)
+
+printf("a is a subset of b ");
+
+           else
+
+printf("a is not a subset of b");			
+		
+	return 0;
+}
